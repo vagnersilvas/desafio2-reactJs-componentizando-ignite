f69d4bf159b435acfd9bd70d3a426ea73a0a1c8e
vagner da silva <vagnrdaia@gmail.com>
2022-02-28 23:08:03 -0300

    inicio component content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+interface MovieProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({movies}: MovieProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {[...movies].map(movie => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
