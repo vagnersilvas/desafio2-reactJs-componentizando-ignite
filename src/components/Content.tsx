1401bc14798dcf7ece8cbce8fe6484d93565ae38
vagner da silva <vagnrdaia@gmail.com>
2022-03-01 22:36:49 -0300

    componente content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,32 +1,58 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 import { Header } from "./Header";
 import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
 interface MovieProps {
-  movies: Array<{
-    imdbID: string;
-    Title: string;
-    Poster: string;
-    Ratings: Array<{
-      Source: string;
-      Value: string;
-    }>;
-    Runtime: string;
-  }>
+
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Value: string;
+  }>;
+  Runtime: string;
 }
 
-export function Content({movies}: MovieProps) {
+export function Content() {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
   return (
-    <main>
-      <div className="movies-list">
-        {[...movies].map(movie => (
-          <MovieCard
-            key={movie.imdbID}
-            title={movie.Title}
-            poster={movie.Poster}
-            runtime={movie.Runtime}
-            rating={movie.Ratings[0].Value}
-          />
-        ))}
-      </div>
-    </main>
+    <div className="container">
+
+      <Header {...selectedGenre}/>
+      <main>
+        <div className="movies-list">
+
+          {movies.map(movie =>
+            <MovieCard
+              key={movie.imdbID}
+              poster={movie.Poster}
+              title={movie.Title}
+              rating={movie.Ratings[0].Value}
+              runtime={movie.Runtime}
+            />)}
+
+        </div>
+      </main>
+    </div>
   )
 }
