5b2d37517fac94ddd82afe6da5fb5534a4e27de7
vagner da silva <vagnrdaia@gmail.com>
2022-03-02 22:21:44 -0300

    add componentes no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
 import './styles/global.scss';
+import { api } from './services/api';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
 
 export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+ 
+  useEffect(() => {
+        api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+  
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar />
 
-      <Content />
+      <SideBar selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId}/>
+
+      <Content selectedGenreId={selectedGenreId} selectedGenre={selectedGenre}/>
     </div>
   )
 }
